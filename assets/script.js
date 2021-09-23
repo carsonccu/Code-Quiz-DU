3533ed090218a1acd372279840c234a768fb3bea
Carson <carsoncolgateccu@gmail.com>
2021-09-23 14:37:52 -0600

    set attributes to have my questions display none and display visible

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var timerEl = document.querySelector(".time");
 var mainElem = document.querySelector("#main");
 var title = document.getElementById("title-page")
-var question1 = document.getElementById("hide1")
-var question2 = document.getElementById("hide2")
-var question3 = document.getElementById("hide3")
+var question1 = document.querySelector("#hide1")
+var question2 = document.querySelector("#hide2")
+var question3 = document.querySelector("#hide3")
 var finalpage = document.getElementById("high-score-page")
 
 var startbutton = document.getElementById("start-button")
@@ -26,7 +26,9 @@ var buttonD3 = document.getElementById("btn-d3")
 
 var secondsleft = 30
 
-
+question1.setAttribute("style", "display: none;")
+question2.setAttribute("style", "display: none;")
+question3.setAttribute("style", "display: none;")
 
 // setting a timer countdown from 30
 var setTime = function () {
@@ -50,3 +52,14 @@ var gameover = function () {
 
 setTime();
 //when countdown to 0 show "game over"
+
+function wrongAnswer() {
+    secondsleft = secondsleft - 5;
+}
+
+startbutton.addEventListener("click", startQuiz)
+
+function startQuiz() {
+    timerEl.classList.remove("time");
+    question1.setAttribute("style", "visibility: visible:")
+}
