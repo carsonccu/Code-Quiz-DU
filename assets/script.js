c33c6ab78acb1b8a23643dbdf7673f3ac2d7087a
Carson <carsoncolgateccu@gmail.com>
2021-09-23 11:09:56 -0600

    added a var for all my answer buttons

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,24 @@
-function timer() {
-    var sec = 30;
-    var timer = setInterval(function () {
-        document.getElementById('timer').innerHTML = '0' + sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
-}
+var timer = document.getElementById("timer")
+var title = document.getElementById("title-page")
+var question1 = document.getElementById("hide1")
+var question2 = document.getElementById("hide2")
+var question3 = document.getElementById("hide3")
+var finalpage = document.getElementById("high-score-page")
+
+var startbutton = document.getElementById("start-button")
+var playagainbutton = document.getElementById("play-again")
+
+var buttonA = document.getElementById("btn-a")
+var buttonB = document.getElementById("btn-b")
+var buttonC = document.getElementById("btn-c")
+var buttonD = document.getElementById("btn-d")
+
+var buttonA2 = document.getElementById("btn-a1")
+var buttonB2 = document.getElementById("btn-b2")
+var buttonA2 = document.getElementById("btn-c2")
+var buttonD2 = document.getElementById("btn-d2")
+
+var buttonA3 = document.getElementById("btn-a3")
+var buttonB3 = document.getElementById("btn-b3")
+var buttonC3 = document.getElementById("btn-c3")
+var buttonD3 = document.getElementById("btn-d3")
